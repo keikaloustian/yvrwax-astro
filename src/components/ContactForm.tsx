23794e56aa02cd42853a2354769feedb94a9f72a
keikaloustian <kei.kaloustian@gmail.com>
2023-04-29 21:28:17 -0700

    Make form React component

---
+++ src/components/ContactForm.tsx
@@ -0,0 +1,25 @@
+export default function Form() {
+  return (
+    <form className="grid">
+      <label>Name</label>
+      <input type="text" className="" placeholder="" />
+
+      <label>Phone</label>
+      <input type="text" className="" placeholder="" />
+
+      <label>Email</label>
+      <input type="text" className="" placeholder="" />
+
+      <label>How many skis/snowboards?</label>
+      <input type="text" className="" placeholder="" />
+
+      <label></label>
+      <input type="checkbox" className="" placeholder="" />
+
+      <label>Message</label>
+      <input type="text" />
+
+      <button>Send</button>
+    </form>
+  );
+}
