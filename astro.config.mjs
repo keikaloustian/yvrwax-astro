c7759927740ab0d64f8647afb26dde0a581d8448
keikaloustian <kei.kaloustian@gmail.com>
2023-04-30 19:52:17 -0700

    Disable automatic css base stylesheet importing and create manual tailwind base stylesheet with all-encompassing focus style overrides

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,15 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image({
-    serviceEntryPoint: "@astrojs/image/sharp"
-  }), react()]
+  integrations: [
+    tailwind({
+      // Example: Disable injecting a basic `base.css` import on every page.
+      // Useful if you need to define and/or import your own custom `base.css`.
+      config: { applyBaseStyles: false },
+    }),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+    react(),
+  ],
 });
