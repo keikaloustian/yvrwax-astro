bcc592b400d698df947e2dcfdbaf443a5c9baac1
keikaloustian <kei.kaloustian@gmail.com>
2023-06-09 15:22:27 -0700

    Add blurred blobs in background

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -18,14 +18,13 @@ module.exports = {
         sans: ["Source Sans Pro", ...defaultTheme.fontFamily.sans],
       },
       keyframes: {
-        verticalFade: {
-          "0%, 100%": { transform: "translate(0, 0)", opacity: 1 },
-          "49%": { transform: "translate(0, 100%)", opacity: 0 },
-          "50%": { transform: "translate(0, -100%)", opacity: 0 },
+        slowFall: {
+          "0%": { transform: "translate(0, -100vh)" },
+          "100%": { transform: "translate(0, 200vh)" },
         },
       },
       animation: {
-        verticalFade: "verticalFade 1.5s ease-in-out",
+        slowFall: "slowFall 5s infinite linear",
       },
     },
   },
